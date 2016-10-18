e0f707f71ac7a5c82da778702afa5c8b75b6e162
michaelsimon <michael.simon@gmail.com>
2016-10-17 21:15:19 -0400

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,15 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
-    });
+describe('MainController', function() {  // describe controllername
+  beforeEach(module('app'));  //before each test, use module app
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){  //inject the controller service given to use by ngMock
+    $controller = _$controller_;  //create custom $scope
+  }));
+  
+  it ('should have Steve Jobs name', function() {
+    var $scope = {};
+
+    var controller = $controller('MainController', { $scope: $scope });
+    expect(controller.name).toEqual('Steve Jobs');
+  })
 });
